dc35e9402109a033fb7f8f88469e0380801f3a2e
Timothy Cyrus <4201229+tcyrus@users.noreply.github.com>
2022-10-29 13:11:06 -0400

    Update extension.py

--- mdp_import/plugin/extension.py
+++ mdp_import/plugin/extension.py
@@ -16,16 +16,18 @@ class MdpExtension(Extension):
     def setup(self) -> None:
         pass
 
-    def importDocument(self) -> None:
-        fileName = QFileDialog.getOpenFileName(filter="mdipack (*.mdp)")[0]
+    def importDocuments(self) -> None:
+        fileNames = QFileDialog.getOpenFileNames(filter="mdipack (*.mdp)")
 
         loader = MdpLoader()
-        with open(fileName, "rb") as _file:
-            doc = loader.buildDoc(_file, self.krita)
 
-        self.krita.activeWindow().addView(doc)
-        doc.refreshProjection()
+        for filename in fileNames:
+            with open(fileName, "rb") as _file:
+                doc = loader.buildDoc(_file, self.krita)
+
+            self.krita.activeWindow().addView(doc)
+            doc.refreshProjection()
 
     def createActions(self, window: Window) -> None:
-        action = window.createAction("mdpImport", "Import MDP File")
-        action.triggered.connect(self.importDocument)
+        action = window.createAction("mdpImport", "Import MDP File(s)")
+        action.triggered.connect(self.importDocuments)
